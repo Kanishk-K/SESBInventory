b33d3bfb90e701ae6ef4109e74b249bea98aa07f
Kanishk-K <kanishkkacholia@gmail.com>
2022-12-21 18:21:48 -0600

    🔐Added simple Google Authentication. Document and verify security.

---
+++ sesb-inventory/pages/api/auth/[...nextauth].js
@@ -0,0 +1,35 @@
+import NextAuth from "next-auth"
+import GoogleProvider from "next-auth/providers/google"
+import { PrismaAdapter } from "@next-auth/prisma-adapter"
+import prisma from "../../../lib/prismadb"
+
+export const authOptions = {
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+  pages:{
+    signIn: '/auth/login'
+  },
+  callbacks: {
+    async signIn({ user, account, profile, email, credentials }){
+      const isAllowedToSignIn = user.isActive || user.isAdmin;
+      if (isAllowedToSignIn){
+        return true
+      }
+      else{
+        return false
+      }
+    },
+    async session({ session, token, user }) {
+      session.isActive = user.isActive
+      session.isAdmin = user.isAdmin
+      return session
+    }
+  }
+}
+
+export default NextAuth(authOptions)
